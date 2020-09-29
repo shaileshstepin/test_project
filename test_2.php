cfd5496c2805ae9b3862607143bf716506f35e1d
Shailesh <shaileshpatel.stepin@gmail.com>
2020-09-29 16:07:58 +0530

    test2 file

---
+++ test_2.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr1 = [9,8,7,2,1,3,100,99,20,-20,-23,-1,21,34]; 
+
+echo "<pre>";
+var_dump($arr1);
+echo "</pre>";
+
+usort($arr1, function($a,$b){
+    return ($a < $b) ? -1 : 1; 
+});
+
+echo "<pre>";
+var_dump($arr1);    
+echo "</pre>";
